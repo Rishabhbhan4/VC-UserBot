ee0a60b8d44f795cd43d4310e5ea5053036d9576
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-01-30 20:58:35 +0530

    Update Skipendpauseresume.py

--- UserBot/Skipendpauseresume.py
+++ UserBot/Skipendpauseresume.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from config import HNDLR, call_py
-from EvaMaria.helpers.decorators import authorized_users_only
-from EvaMaria.helpers.handlers import skip_current_song, skip_item
-from EvaMaria.helpers.queues import QUEUE, clear_queue
+from UserBot.helpers.decorators import authorized_users_only
+from UserBot.helpers.handlers import skip_current_song, skip_item
+from UserBot.helpers.queues import QUEUE, clear_queue
 
 
 @Client.on_message(filters.command(["skip"], prefixes=f"{HNDLR}"))
